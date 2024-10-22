0af6eae0f9e3eda123467db253b0429d346a18cb
Cassidy Waldrip <cassidy.waldrip@gmail.com>
2024-10-21 22:32:20 -0400

    Create netsci package

---
+++ src/netsci_toolkit/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present Cassidy Waldrip <cassidy.waldrip@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Cassidy Waldrip <cassidy.waldrip@gmail.com>
+#
+# SPDX-License-Identifier: MIT
